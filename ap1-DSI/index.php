b24769eee3715996ad607331a934825916060c65
Kevilin Rodrigues <62702790+Kevilin@users.noreply.github.com>
2022-09-29 10:59:50 -0300

    Add files via upload

---
+++ ap1-DSI/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/addons/p5.sound.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <meta charset="utf-8" />
+</head>
+
+<body>
+  <div class="container">
+    <script src="sketch.js"></script>
+    <p id="container">Arraste uma imagem para o Canvas abaixo:</p>
+    <p id="prediction"></p>
+  </div>
+</body>
+
+<script src="https://unpkg.com/ml5@0.4.3/dist/ml5.min.js"></script>
+
+</html>
